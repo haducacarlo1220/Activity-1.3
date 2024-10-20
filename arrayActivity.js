0343b31d88db2af09e4b87b86ce7c432420abf4b
haducacarlo1220 <ralphhaduca@gmail.com>
2024-10-20 22:23:55 +0800

    Initial commit

---
+++ arrayActivity.js
@@ -0,0 +1,13 @@
+let favSingers = ["Adele", "Ed Sheeran", "Taylor Swift"];
+
+console.log("First favorite singer:", favSingers[0]);
+
+let favNumbers = [7, 22, 3, 15];
+
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log("Mixed array items:");
+console.log("Item 0:", mixedArr[0]); 
+console.log("Item 1:", mixedArr[1]); 
+console.log("Item 2:", mixedArr[2]); 
+console.log("Item 3:", mixedArr[3]); 
